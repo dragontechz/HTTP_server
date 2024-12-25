adb138f7f481a67d86cee499d9498e7351976d1c
dragontechz <juleskono58@gmail.com>
2024-12-25 07:09:24 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
+json_content=$(cat <<-EOF
+[Unit]
+Description=Daemon HTTP Tunnel Server
+Wants=network.target
+After=network.target
 
-./server &
+[Service]
+ExecStart=/root/HTTP_server/server
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)
+
+echo "$json_content" > /etc/systemd/system/server.service
+systemctl daemon-reload
+systemctl start server
+systemctl enable server
+
+echo "HTTP Proxy installed successfully"
+exit 1
