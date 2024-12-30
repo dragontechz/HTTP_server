53c1d629e73a44a9adf495cadc1f7ff140fd613e
dragontechz <juleskono58@gmail.com>
2024-12-30 19:22:48 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,66 +1,26 @@
+json_content=$(cat <<-EOF
+[Unit]
+Description=Daemonize ASH HTTP Tunnel Server
+Wants=network.target
+After=network.target
 
-read -p "Saisissez le numéro du port : " port
+[Service]
+ExecStart=/root/HTTP_server/server
+Restart=always
+RestartSec=3
 
-# Vérifie si le numéro saisi est un nombre
-if (($port >= 1 && $port <= 65535)); then
-  # Si c'est un numéro, affiche une message de confirmation
-  echo "Vous avez saisi un numéro valide pour le port : $port"
-  json_content=$(cat <<-EOF
-  [Unit]
-  Description=Daemonize ASH HTTP Tunnel Server
-  Wants=network.target
-  After=network.target
-  
-  [Service]
-  ExecStart=/root/HTTP_server/server -p $port
-  Restart=always
-  RestartSec=3
-  
-  [Install]
-  WantedBy=multi-user.target
+[Install]
+WantedBy=multi-user.target
 EOF
 )
-  echo "$json_content" > /etc/systemd/system/server.service
-  systemctl daemon-reload
-  systemctl start server
-  systemctl enable server
-  lsof -i :"$port"
-  echo -e "$YELLOW"
-  echo "HTTP Proxy installed successfully"
-  echo -e "$NC"
-  exit 1
-fi
 
-
-else
-  
-  echo "utilisation du port par defaut 8080"
-  json_content=$(cat <<-EOF
-  [Unit]
-  Description=Daemonize ASH HTTP Tunnel Server
-  Wants=network.target
-  After=network.target
-  
-  [Service]
-  ExecStart=/root/HTTP_server/server
-  Restart=always
-  RestartSec=3
-  
-  [Install]
-  WantedBy=multi-user.target
-EOF
-)
-  echo "$json_content" > /etc/systemd/system/server.service
-
-
-
+echo "$json_content" > /etc/systemd/system/server.service
 systemctl daemon-reload
 systemctl start server
 systemctl enable server
 
-lsof -i :"$port"
+lsof -i :"$http_port"
 echo -e "$YELLOW"
 echo "HTTP Proxy installed successfully"
 echo -e "$NC"
 exit 1
-fi
